c4d7b3a0ab73e286de107f6edfabf3f35ea73c19
poireau <kovalkov1899@mail.ru>
2024-03-11 09:21:23 +0300

    fixes

--- Zovprofil/zovprofil.cs
+++ Zovprofil/zovprofil.cs
@@ -67,11 +67,11 @@ namespace Zovprofil
 
     public class Catalog
     {
-        public static string ConnectionString = "Data Source=localhost;Initial Catalog=infiniu2_catalog;Persist Security Info=True;Connection Timeout=30;User ID=infiniu2_infinium;Password=InF476()*";
-        public static string ftpPath = "ftp://localhost/Documents/TechStoreDocuments/";
+        //public static string ConnectionString = "Data Source=localhost;Initial Catalog=infiniu2_catalog;Persist Security Info=True;Connection Timeout=30;User ID=infiniu2_infinium;Password=InF476()*";
+        //public static string ftpPath = "ftp://localhost/Documents/TechStoreDocuments/";
 
-        //public static string ConnectionString = "Data Source=185.204.118.40, 32433;Initial Catalog=infiniu2_catalog;Persist Security Info=True;Connection Timeout=30;User ID=infiniu2_infinium;Password=InF476()*";
-        //public static string ftpPath = "ftp://infinium.zovprofil.by/Documents/TechStoreDocuments/";
+        public static string ConnectionString = "Data Source=185.204.118.40, 32433;Initial Catalog=infiniu2_catalog;Persist Security Info=True;Connection Timeout=30;User ID=infiniu2_infinium;Password=InF476()*";
+        public static string ftpPath = "ftp://infinium.zovprofil.by/Documents/TechStoreDocuments/";
 
 
         public static string URL = "https://zovprofil.by/Images/ClientsCatalogImages/";
@@ -101,7 +101,7 @@ namespace Zovprofil
 
                         foreach (DataRow Row in DT.Rows)
                         {
-                            string query = "SELECT TOP 1 FileName FROM ClientsCatalogImages WHERE Category = '" + Row["Category"].ToString() + "' AND ProductType != 3 AND ToSite = 1 AND Basic = 1";
+                            string query = "SELECT TOP 1 FileName FROM ClientsCatalogImages WHERE Category = '" + Row["Category"].ToString() + "' AND ProductType != 3 AND ToSite = 1 AND Basic = 1 ORDER BY Color DESC";
 
                             using (SqlDataAdapter sDA = new SqlDataAdapter(query, ConnectionString))
                             {
